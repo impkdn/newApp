3dcf9d5b927badf97daaaea0eb45e10ee6a0c65d
impkdn <piyush.jld@gmail.com>
2022-08-17 15:08:37 +0530

    dashboard is going on

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,13 +3,14 @@ console.log("Hi");
 function handleLogin(e){
     e.preventDefault();
     var isValid = true;
-    var emailValidation = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    var passwordValidation = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    var emailValidationRegex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    var passwordValidationRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
     var minNumberofChars = 6;
     var userEmail = document.getElementById("u-email");
     // console.log(userEmail);
     var password = document.getElementById("psw");
     var loader = document.querySelector("#loader");
+    var formWrapper = document.querySelector(".from-wrapper");
     function displayLoader() {
         loader.classList.add("display");
         setTimeout(() => {
@@ -19,16 +20,18 @@ function handleLogin(e){
     function hideLoader() {
         loader.classList.remove("display")
     }
-    if ( !emailValidation.test(userEmail.value)) {
+    if ( !emailValidationRegex.test(userEmail.value)) {
         alert("Please enter valid Email")
         isValid = false;       
     };
-    if (!passwordValidation.test(password.value) || password < minNumberofChars) {
+    if (!passwordValidationRegex.test(password.value) || password < minNumberofChars) {
         alert("Your Password must contain atleast one special character, atleast one number and must be greater than or equal to 6 characters");
         isValid = false;
     }
     if (isValid) {
         alert("Form data is valid");
+        formWrapper.classList.add("display-none");
+
         displayLoader()
 
         fetch('https://api.publicapis.org/entries')
@@ -53,33 +56,13 @@ function handleLogin(e){
         document.getElementById("item").innerHTML = item;
         }
 
-        function showAll() {
-            displayData(fetchedData, 200)
-            console.log()
-        }
+        // function showAll() {
+        //     displayData(fetchedData, 200)
+        //     console.log()
+        // }
     
     }else {
         alert("Form data is invalid");
     }
 };
 
-// function validateLogin () {
-//     var userEmail = document.getElementById("u-email");
-//     console.log(userEmail.value);
-//     var password = document.getElementById("psw");
-//     var minNumberofChars = 6;
-//     console.log(password.value); 
-//     var emailValidation = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-//     var passwordValidation = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-
-//     if ( !emailValidation.test(userEmail.value)) {
-//         alert("Please enter valid Email")       
-//     };
-//     if (!passwordValidation.test(password.value) || password.value < minNumberofChars) {
-//         alert("Your Password must contain atleast one special character, atleast one number and must be greater than or equal to 6 characters");
-//     } else {
-
-//     };
-
-
-// };
