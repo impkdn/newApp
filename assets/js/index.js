11c8801f4b032dd62722f48e28bd891f16d4c96b
impkdn <piyush.jld@gmail.com>
2022-08-19 00:21:59 +0530

    ppublic api fetch is done, search is done , sorting is implemented but not working

--- assets/js/index.js
+++ assets/js/index.js
@@ -59,28 +59,34 @@ function handleLogin(e){
 
 
         function displayData(data, total = 4) {
-            fetchedData = data;
+            fetchedData = data.entries;
             console.log(fetchedData)
             let item = '';
             let txt = "";
+            var myTable = document.querySelector(".item-table");
+            fetchedData.forEach( emp => {
+                var tableRow = document.createElement('tr');
+                Object.values(emp).forEach(text => {
+                    let cell = document.createElement('td');
+                    let textNode = document.createTextNode(text);
+                    cell.appendChild(textNode);
+                    tableRow.appendChild(cell);
+                });
 
-        for (let i = 0; i < total; i++) {
-            var objectData = data.entries[i];
-            console.log(objectData);
-            var newRow = document.createElement("tr");
-            newRow.classList.add("myRow");
-            for (let x in objectData) {
-                txt += `<td>${objectData[x]}</td>`;
-                newRow.appendChild(txt);
-                console.log(newRow.innerHTML);
-                var items = document.getElementById("item").innerHTML;
-                // items.appendChild(newRow)
-            };
-            // for (let j = 0; j < 7; j++) {
-            // item += ``
-            //  }
-            // item += `<td>${data.entries[i].API}</td>`;
-        }
+                myTable.appendChild(tableRow);
+            });
+        // for (let i = 0; i < total; i++) {
+        //     var objectData = data.entries[i];
+        //     console.log(objectData);
+
+
+        //     for (let x in objectData) {
+        //         txt += `<td>${objectData[x]}</td>`;
+        //         document.getElementById("item").innerHTML = txt;
+                
+        //     };
+           
+        // }
         }
 
         // function showAll() {
@@ -93,4 +99,79 @@ function handleLogin(e){
     }
     
 };
+function searchFromTable() {
+    let input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.querySelector(".item-table");
+    tr = document.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+
+        
+    }
+}
+function sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.querySelector(".item-table");
+    switching = true;
+    //Set the sorting direction to ascending:
+    dir = "asc"; 
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+      //start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+    //   console.log(rows);
+      /*Loop through all table rows (except the
+      first, which contains table headers):*/
+      for (i = 1; i < (rows.length - 1); i++) {
+        //start by saying there should be no switching:
+        shouldSwitch = false;
+        /*Get the two elements you want to compare,
+        one from current row and one from the next:*/
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /*check if the two rows should switch place,
+        based on the direction, asc or desc:*/
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch= true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /*If a switch has been marked, make the switch
+        and mark that a switch has been done:*/
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        //Each time a switch is done, increase this count by 1:
+        switchcount ++;      
+      } else {
+        /*If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again.*/
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
 
